c21372cda67401f23951ca8880cae9086f3b8739
samirmaciel <samirmaciel.dev@gmail.com>
2021-11-29 00:00:24 -0300

    Created Notes Screen

--- app/src/main/java/com/samirmaciel/notestosave/ui/theme/Theme.kt
+++ app/src/main/java/com/samirmaciel/notestosave/ui/theme/Theme.kt
@@ -3,43 +3,24 @@ package com.samirmaciel.notestosave.ui.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color.Companion.White
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = White,
+    background = DarkGray,
+    onBackground = White,
+    surface = LightBlue,
+    onSurface = DarkGray
 )
 
 @Composable
 fun NotesToSaveTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = true,
     content: @Composable() () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
     MaterialTheme(
-        colors = colors,
+        colors = DarkColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
